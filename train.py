d37004c7d6d4376d4452631c40039637102c65e8
Compute-Chem <58101737+xianHUJINGJING@users.noreply.github.com>
2023-11-23 22:56:36 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,96 +1,19 @@
-from __future__ import division
-from __future__ import print_function
-
 import time
-import numpy as np
-import random
-import torch
 import torch.nn.functional as F
 import pandas as pd
 import matplotlib.pyplot as plt
-from torch_geometric.loader import DataLoader
 import copy
 from sklearn.metrics import r2_score
 import os
-import logging
-import sys
+
 os.environ['CUDA_LAUNCH_BLOCKING'] = '1'
 
 import torch
 from src.transformer import make_model
 from src.featurization.data_utils import load_data_from_df, construct_loader
+from src.utils import setup_logger, dict_to_markdown, L2Loss, set_seed, rmse, test
+from config import args, model_params
 
-def setup_logger(name, save_dir, filename="training_log.txt"):
-    logger = logging.getLogger(name)
-    logger.setLevel(logging.DEBUG)
-
-    ch = logging.StreamHandler(stream=sys.stdout)
-    ch.setLevel(logging.DEBUG)
-    formatter = logging.Formatter("%(asctime)s %(name)s %(levelname)s: %(message)s")
-    ch.setFormatter(formatter)
-    logger.addHandler(ch)
-
-    if save_dir:
-        fh = logging.FileHandler(os.path.join(save_dir, filename))
-        fh.setLevel(logging.DEBUG)
-        fh.setFormatter(formatter)
-        logger.addHandler(fh)
-        logger.propagate = False
-
-    return logger
-
-def dict_to_markdown(d, max_str_len=120):
-    # convert list into its str representation
-    # d = {k: v.__repr__() if isinstance(v, list) else v for k, v in d.items()}
-    # truncate string that is longer than max_str_len
-    if max_str_len is not None:
-        d = {k: v[-max_str_len:] if isinstance(v, str) else v for k, v in d.items()}
-    return pd.DataFrame(d, index=[0]).transpose().to_markdown()
-
-
-class L2Loss(torch.nn.Module):
-    def __init__(self):
-        super(L2Loss, self).__init__()
-
-    def forward(self, y_pred, y_true):
-        squared_errors = (y_pred - y_true) ** 2
-        return torch.mean(squared_errors)
-
-
-def set_seed(seed):
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
-    np.random.seed(seed)
-    random.seed(seed)
-
-model_names = {
-    1: 'MAT',
-}
-
-args = {
-    'model': model_names[1],                                                
-    'save_dir': './results_right_ring1',
-    'train_data_path': r"C:\Users\Lenovo\PycharmProjects\Trans_R\data\newdataring\ring_ox_train.csv",
-    'test_data_path': r"C:\Users\Lenovo\PycharmProjects\Trans_R\data\newdataring\ring_ox_test.csv",
-    'pretained_path': "/gpfs/home/C22201088/ssz/Trans_R/results_new_translongnew/model.pt",   
-
-    'train_num': 'all',            # input 1000 or 'all'
-    'test_num': 'all',
-    'pretrained':False,                                       
-    'noise': True,                                         
-    'noise_std': 0.0,
-    'gpu': 0,                                                 
-    'epochs': 230,
-    'es_patience': 50,
-    'lr': 0.0001,                                               
-    'step_size': 10,                                            
-    'gamma': 0.96,                                              
-    'batch_size': 64,                                          
-    'num_workers': 32,                                       
-    'seed': 42,                                                
-}
 
 set_seed(args['seed'])
 model_path = os.path.join(args['save_dir'], 'model.pt')
@@ -99,50 +22,19 @@ loss_path = os.path.join(args['save_dir'], 'train_loss.csv')
 loss_img = os.path.join(args['save_dir'], 'loss.png')
 pred_path = os.path.join(args['save_dir'], 'predict.csv')
 
+if not os.path.exists(args['save_dir']):
+    os.makedirs(args['save_dir'])
+
+
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 if torch.cuda.is_available():
     torch.cuda.set_device(args['gpu'])
 
 
-def rmse(preds, labels):
-    return torch.sqrt(torch.mean((preds - labels) ** 2))
-
-
-def test(model, loader):
-    model.eval()
-    model.cuda()
-    index_ls = []
-    pred_ls = []
-    y_ls = []
-    for batch in loader:
-        with torch.no_grad():
-            adjacency_matrix, node_features, distance_matrix, sp3_mask, y, index = batch
-            batch_mask = torch.sum(torch.abs(node_features), dim=-1) != 0
-            node_features = node_features.cuda()
-            batch_mask = batch_mask.cuda()
-            adjacency_matrix = adjacency_matrix.cuda()
-            distance_matrix = distance_matrix.cuda()
-            pred = model(node_features, batch_mask, adjacency_matrix, distance_matrix, None)
-
-            pred_ls.append(pred)
-            y_ls.append(y)
-            index_ls.append(index)
-
-    pred = torch.cat(pred_ls, dim=0).reshape(-1).cuda()  # (batch_size, 1)
-    y = torch.cat(y_ls, dim=0).reshape(-1).cuda()
-    index = torch.cat(index_ls, dim=0).reshape(-1).cuda()
-
-    mae = F.l1_loss(pred.reshape(-1), y.reshape(-1))
-    test_loss = mae
-    rmse = torch.sqrt(test_loss)
-
-    return test_loss, mae, rmse, y, pred, index
-
 
 if __name__ == '__main__':
 
-    if not os.path.exists(args['save_dir']):
-        os.makedirs(args['save_dir'])
+
     logger = setup_logger(f"{args['model']}", args['save_dir'])
     logger.info('-' * 60)
 
@@ -155,24 +47,12 @@ if __name__ == '__main__':
     test_X, test_y = load_data_from_df(args['test_data_path'], num=args['test_num'], one_hot_formal_charge=True)
     test_loader = construct_loader(test_X, test_y, args['batch_size'])
 
+    print(train_d_atom, test_X[0][0].shape[1])
+
     logger.info(f'The train dataset contains {len(train_X)} samples.')
     logger.info(f'The test dataset contains {len(test_X)} samples.')
 
-    model_params = {
-        'd_atom': train_d_atom,
-        'd_model': 1024,
-        'N': 8,
-        'h': 16,
-        'N_dense': 1,
-        'trainable_lambda': False,
-        'lambda_attention': 0.5,
-        'lambda_distance': 0.,
-        'leaky_relu_slope': 0.1,
-        'dense_output_nonlinearity': 'relu',
-        'distance_matrix_kernel': 'exp',
-        'dropout': 0.1,
-        'aggregation_type': 'mean'
-    }
+
 
     logger.info('The model params are:\n' + f'{dict_to_markdown(model_params, max_str_len=120)}')
 
@@ -224,7 +104,7 @@ if __name__ == '__main__':
             sp3_mask = sp3_mask.cuda()
 
             if args['noise']:
-                torch.manual_seed(epoch)  # 设置PyTorch随机种子
+                torch.manual_seed(epoch) 
                 noise = torch.randn_like(distance_matrix) * args['noise_std'] * sp3_mask
                 noise = noise.cuda()
                 distance_matrix = distance_matrix + noise
@@ -314,6 +194,3 @@ if __name__ == '__main__':
     plt.plot(loss_data)
     plt.savefig(loss_img)
     plt.show()
-
-
-
